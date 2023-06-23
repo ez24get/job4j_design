b07d9356c27ac09c3b8c87c854b370ef3d155a3f
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-06-23 14:04:16 +0300

    3. Утверждения с примитивными типами [#504883]

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,72 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.DecimalFormat;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisTetrahedron() {
+        Box box = new Box(4, 20);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void isThisUNKNOWN() {
+        Box box = new Box(5, 110);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Unknown object");
+    }
+
+    @Test
+    void tetrahedronSquare() {
+        Box box = new Box(4, 10);
+        double tSquare = box.getArea();
+        assertThat(new DecimalFormat("#.##").format(tSquare)).isEqualTo("173,21");
+    }
+
+    @Test
+    void sphereSquare() {
+        Box box = new Box(0, 10);
+        double tSquare = box.getArea();
+        assertThat(new DecimalFormat("#.##").format(tSquare)).isEqualTo("1256,64");
+    }
+
+    @Test
+    void numberOfVertex() {
+        Box box = new Box(4, 10);
+        int vertex = box.getNumberOfVertices();
+        assertThat(vertex).isEqualTo(4);
+    }
+
+    @Test
+    void numberOfVertexWrong() {
+        Box box = new Box(5, 10);
+        int vertex = box.getNumberOfVertices();
+        assertThat(vertex).isEqualTo(-1);
+    }
+
+    @Test
+    void figureNotExists() {
+        Box box = new Box(7, 10);
+        boolean exist = box.isExist();
+        assertThat(exist).isFalse();
+    }
+
+    @Test
+    void figureExists() {
+        Box box = new Box(4, 10);
+        boolean exist = box.isExist();
+        assertThat(exist).isTrue();
+    }
+}
