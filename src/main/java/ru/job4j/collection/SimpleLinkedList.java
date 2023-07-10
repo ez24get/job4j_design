cddb7b02b58bf9823dd4410922eb31460800e163
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-07-10 19:09:57 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,69 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+
+    private int size = 0;
+    private int modCount = 0;
+    private Node<E> head;
+
+    private static class Node<E> {
+        private E item;
+        private Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        Node<E> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            head.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> rsl = head;
+        for (int i = 0; i < index; i++) {
+            rsl = rsl.next;
+        }
+        return rsl.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            Node<E> node = head;
+            final int expectedModcount = modCount;
+            int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModcount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return node != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("No element found");
+                }
+                E value = node.item;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+}
