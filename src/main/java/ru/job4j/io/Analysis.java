f5da8ab821e925b9a045896683155c745568ea51
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-09-08 10:36:23 +0300

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,45 +1,22 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
 
-    private String offline;
-
     public void unavailable(String source, String target) {
-        try (BufferedReader br = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(source));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
             String line;
             boolean serverOn = true;
-            List<String> output = new ArrayList<>();
-            StringBuilder result = new StringBuilder();
             while (br.ready()) {
                 line = br.readLine();
-                if (!line.trim().isEmpty()) {
-                    if ((line.startsWith("400") || line.startsWith("500")) && serverOn) {
-                        offline = line.split(" ")[1];
-                        serverOn = false;
-                    } else if ((line.startsWith("200") || line.startsWith("300")) && !serverOn) {
-                        String online = line.split(" ")[1];
-                        result.append(String.format("%s;%s;%s", offline,
-                                online, System.lineSeparator()));
-                        output.add(result.toString());
-                        result = new StringBuilder();
-                        serverOn = true;
-                    }
+                if (serverOn == (line.startsWith("400") || line.startsWith("500"))) {
+                    writer.append(line.substring(4))
+                            .append(';')
+                            .append(serverOn ? "" : System.lineSeparator());
+                    serverOn = !serverOn;
                 }
-            }
-            writeToTarget(output, target);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-
-    private void writeToTarget(List<String> input, String target) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
-            for (String line : input) {
-                writer.write(line);
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
