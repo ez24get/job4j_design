6286a3b35dc57dab3a8377545615972c06df34d9
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-09-25 14:18:45 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,22 +1,24 @@
 package ru.job4j.io.duplicates;
 
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Set;
-import java.util.function.Predicate;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.equals(new FileProperty(1200, "my_photo.jpeg"))).forEach(System.out::println);
-    }
-
-    public static Set<Path> search(Path root, Predicate<FileProperty> condition) throws IOException {
-        DuplicatesVisitor visitor = new DuplicatesVisitor(condition);
-        Files.walkFileTree(root, visitor);
-        return visitor.getPathSet();
+        Path start = Path.of(".");
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Files.walkFileTree(start, visitor);
+        Map<FileProperty, List<Path>> duplicates = visitor.getDuplicates();
+        for (Map.Entry<FileProperty, List<Path>> entry : duplicates.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                System.out.println(entry.getKey().getName() + " " + entry.getKey().getSize());
+                for (Path path : entry.getValue()) {
+                    System.out.println("  " + path.toAbsolutePath());
+                }
+            }
+        }
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,36 +1,27 @@
 package ru.job4j.io.duplicates;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
-import java.util.function.Predicate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private Predicate<FileProperty> condition;
-    private List<Path> paths = new ArrayList<>();
-    private Set<Path> pathSet = new HashSet<>();
-
-    public DuplicatesVisitor(Predicate<FileProperty> condition) {
-        this.condition = condition;
-    }
+    private Map<FileProperty, List<Path>> duplicates = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        boolean isFile = Files.isRegularFile(file);
         FileProperty property = new FileProperty(attrs.size(), file.getFileName().toString());
-        if (isFile && condition.test(property)) {
-            paths.add(file);
-        }
+        List<Path> paths = duplicates.getOrDefault(property, new ArrayList<>());
+        paths.add(file);
+        duplicates.put(property, paths);
         return super.visitFile(file, attrs);
     }
 
-    public Set<Path> getPathSet() {
-        pathSet.addAll(paths);
-        return pathSet;
+    public Map<FileProperty, List<Path>> getDuplicates() {
+        return duplicates;
     }
 }
