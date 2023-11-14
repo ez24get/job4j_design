e976048f8e1c5d5b09efbf26ce8aa2b47c06530b
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-11-14 18:18:46 +0300

    16. Кодировка [#862 #459560]

---
+++ src/main/java/ru/job4j/io/consoleChat/ConsoleChat.java
@@ -0,0 +1,86 @@
+package ru.job4j.io.consoleChat;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+    private final List<String> phrases = new ArrayList<>();
+    private List<String> chatLog = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        System.out.println("Введите текст: ");
+        phrases.addAll(readPhrases());
+        boolean pause = true;
+        while (!scanner.hasNext(OUT)) {
+            if (scanner.hasNext(STOP)) {
+                System.out.println(" ");
+                chatLog.add("-" + STOP);
+                pause = true;
+                askQuestion();
+            } else if (pause && scanner.hasNext(CONTINUE)) {
+                chatLog.add("-" + CONTINUE);
+                int answer = new Random().nextInt(3);
+                System.out.println(phrases.get(answer));
+                pause = false;
+                askQuestion();
+            } else {
+                String answer = giveAnswer();
+                chatLog.add("-" + answer);
+                System.out.println(answer);
+                askQuestion();
+            }
+        }
+        saveLog(chatLog);
+    }
+
+    private String askQuestion() {
+        System.out.println(":");
+        return scanner.nextLine();
+    }
+
+    private String giveAnswer () {
+        int answer = new Random().nextInt(3);
+        return phrases.get(answer);
+    }
+
+    private List<String> readPhrases() {
+        List<String> answers = new ArrayList<>();
+        try (BufferedReader read = new BufferedReader(new FileReader(botAnswers, StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = read.readLine()) != null) {
+                answers.add(Arrays.toString(line.split("\n")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return answers;
+    }
+
+    private void saveLog(List<String> log) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, StandardCharsets.UTF_8))) {
+            for (String line : log) {
+                bufferedWriter.write(line + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("/Users/michael/job4j_design/src/main/java/ru/job4j/io/consoleChat/ChatLog.txt",
+                "/Users/michael/job4j_design/src/main/java/ru/job4j/io/consoleChat/Phrases.txt");
+        cc.run();
+    }
+}
