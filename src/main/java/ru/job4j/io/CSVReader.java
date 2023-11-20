2a540ac2f34a3afe4156227631cd6c2b3bb6a24b
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-11-20 20:18:52 +0300

    17. Scanner [#504791 #460679]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -7,22 +7,20 @@ import java.util.*;
 
 public class CSVReader {
 
-    private static List<String> input = new ArrayList<>();
-    private static List<String> headLine = new ArrayList<>();
-    private static List<String> headLineOut = new ArrayList<>();
-    private static List<String> bodyOut = new ArrayList<>();
-    private static List<String> filters = new ArrayList<>();
-    private static List<Integer> index = new ArrayList<>();
-    private static List<String> output = new ArrayList<>();
-
     public static void handle(ArgsName argsName) throws Exception {
-        getFilters(argsName);
-        var scanner = new Scanner(new FileInputStream(argsName.get("path")))
-                .useDelimiter(System.lineSeparator());
-        while (scanner.hasNext()) {
-            input.add(scanner.next());
+        List<String> input = new ArrayList<>();
+        List<String> headLineOut = new ArrayList<>();
+        List<String> bodyOut = new ArrayList<>();
+        List<Integer> index = new ArrayList<>();
+        String[] filterParts = argsName.get("filter").split(",");
+        List<String> filters = new ArrayList<>(Arrays.asList(filterParts));
+        try (var scanner = new Scanner(new FileInputStream(argsName.get("path")))
+                .useDelimiter(System.lineSeparator())) {
+            while (scanner.hasNext()) {
+                input.add(scanner.next());
+            }
         }
-        headLine.addAll(List.of(input.get(0).split(";")));
+        List<String> headLine = new ArrayList<>(List.of(input.get(0).split(argsName.get("delimiter"))));
         for (int i = 0; i < headLine.size(); i++) {
             String line = headLine.get(i);
             for (String filter : filters) {
@@ -33,33 +31,24 @@ public class CSVReader {
             }
         }
         for (int i = 1; i < input.size(); i++) {
-            String[] bodyPart = input.get(i).split(";");
+            String[] bodyPart = input.get(i).split(argsName.get("delimiter"));
             for (int j = 0; j < index.size(); j++) {
                 if (j < index.size() - 1) {
-                    bodyOut.add(bodyPart[index.get(j)]);
+                    bodyOut.add(bodyPart[index.get(j)] + argsName.get("delimiter"));
                 } else {
                     bodyOut.add(bodyPart[index.get(j)] + System.lineSeparator());
                 }
             }
         }
+        String head = String.join(argsName.get("delimiter"), headLineOut) + System.lineSeparator();
+        String body = String.join("", bodyOut);
+        String outFinal = head + body;
         try (PrintStream stream = new PrintStream(new FileOutputStream(argsName.get("out")))) {
-            stream.print(combineHeadAndBody());
+            stream.print(outFinal);
         }
     }
 
-    private static void getFilters(ArgsName argsName) {
-        String[] filterParts = argsName.get("filter").split(";");
-        filters.addAll(Arrays.asList(filterParts));
-    }
-
-    private static String combineHeadAndBody() {
-        String head = String.join(";", headLineOut) + System.lineSeparator();
-        String body = String.join(";", bodyOut);
-        return head + body;
-    }
-
     public static void main(String[] args) throws Exception {
-        /* здесь добавьте валидацию принятых параметров*/
         ArgsName argsName = ArgsName.of(args);
         handle(argsName);
     }
