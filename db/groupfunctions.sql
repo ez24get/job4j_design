7c47c518f0ca63bf56488a57e3be1684eb9997f9
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-12-08 12:05:33 +0300

    2. Группировка и агрегатные функции [#373618 #465215]

--- db/groupfunctions.sql
+++ db/groupfunctions.sql
@@ -15,17 +15,27 @@ create table devices_people(
     people_id int references people(id)
 );
 
-insert into devices(name, price) values('samsung', 200.5), ('apple', 250.5), ('xiaomi', 150.3);
+insert into devices(name, price) values('samsung', 2000.5), ('apple', 5000.5), ('xiaomi', 1500.3);
 insert into people(name) values('Igor'), ('Maria'), ('Dmitry'), ('Olga');
-insert into devices_people(device_id, people_id) values(1, 1), (1, 2), (1, 3);
+insert into devices_people(device_id, people_id) values(1, 1), (2, 2), (1, 3);
 insert into devices_people(device_id, people_id) values(2, 1), (2, 2), (3, 3);
-insert into devices_people(device_id, people_id) values(3, 1), (4, 2), (4, 3);
+insert into devices_people(device_id, people_id) values(3, 1), (2, 2), (3, 4);
 
 select avg(price) from devices;
 
 select people.name, avg(devices.price) 
-join people.name 
-on devices_people.people_id = people.id 
-join devices.name 
-on devices_people.device_id = devices.id 
+from devices_people as dp 
+join devices 
+on dp.device_id = devices.id 
+join people 
+on dp.people_id = people.id 
 group by people.name;
+
+select people.name, avg(devices.price) 
+from devices_people as dp 
+join devices 
+on dp.device_id = devices.id 
+join people 
+on dp.people_id = people.id 
+group by people.name 
+having avg(devices.price) > 5000;
