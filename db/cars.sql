6b005a0c4940073d2f63c1ebfc640420894b4b1c
michaelberezovski <berezovskii.mikhail@gmail.com>
2023-12-19 11:42:32 +0300

    1. Хранилище машин [#1733]

--- db/cars.sql
+++ db/cars.sql
@@ -15,30 +15,38 @@ create table car_transmissions(
 
 create table cars(
 	id serial primary key, 
-	name text
-	body_id references car_bodies(id), 
-	engine_id references car_engines(id),
-	transmission_id references car_transmissions(id)
+	name text, 
+	body_id int references car_bodies(id), 
+	engine_id int references car_engines(id), 
+	transmission_id int references car_transmissions(id)
 );
 
-insert into car_bodies name values('coupe', 'cabrio', 'sedan', 'pick-up');
-insert into car_engines name values('v6', 'v8', 'w12', 'i4');
-insert into car_transmissions name values('6', '8', 'variator', 'manual');
-insert into cars name, body_id, engine_id, transmission_id 
-values ('toyota rav4', 'bmw 320d', 'audi a5', 'bmw 640 coupe'), 
-(1, 4, 4, 3), 
-(2, 3, 3, 3), 
-(4, 3, 1, 1);
+insert into car_bodies (name) values ('coupe'), ('cabrio'), ('sedan'), ('pick-up');
+insert into car_engines (name) values ('v6'), ('v8'), ('w12'), ('i6');
+insert into car_transmissions (name) values ('6'), ('8'), ('variator'), ('manual');
+insert into cars (name, body_id, engine_id, transmission_id) 
+values ('toyota rav4', 4, 4, 3), 
+('bmw 320d', 3, 3, 3), 
+('audi a5', 3, 1, 1), 
+('bmw 640 coupe', 2, 3, 3);
 
-select d.id as id, 
-d.name as car_name, 
-o.name as body_name, 
-e.name as engine_name  
-from cars d 
-full join car_bodies o, car_engines e, car_transmissions f 
-on d.body_id = o.id, d.engine_id = e.id, d.transmission_id = f.id;
+select cars.id, cars.name as car_name, car_bodies.name as body_name, car_engines.name as engine_name, car_transmissions.name as transmission_name
+from cars
+full join car_bodies on cars.body_id = car_bodies.id
+full join car_engines on cars.engine_id = car_engines.id
+full join car_transmissions on cars.transmission_id = car_transmissions.id;
 
-select * from cars d 
-left join car_bodies o 
-on d.body_id = o.id
+select * from car_bodies d 
+left join cars o 
+on o.body_id = d.id
+where o.id is null;
+
+select * from car_engines d 
+left join cars o 
+on o.engine_id = d.id
+where o.id is null;
+
+select * from car_transmissions d 
+left join cars o 
+on o.transmission_id = d.id
 where o.id is null;
